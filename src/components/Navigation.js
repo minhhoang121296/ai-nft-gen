42d1012f1d243d5046c48f3ae1cdd10f03458355
Howard <phoang1212996@gmail.com>
2023-05-31 23:48:15 +0700

    init

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+
+const Navigation = ({ account, setAccount }) => {
+    const connectHandler = async () => {
+    }
+
+    return (
+        <nav>
+            <div className='nav__brand'>
+                <h1>AI NFT Generator</h1>
+            </div>
+
+         
+        </nav>
+    );
+}
+
+export default Navigation;
